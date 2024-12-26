518c2a9e721006f3aa924db75c9776dafdaab014
Pratham Yadav <prathamyadav05@gmail.com>
2024-12-26 21:44:46 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,8 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <BrowserRouter basename="/Portfolio/">
+    </BrowserRouter>
     <App />
   </React.StrictMode>,
 )
